bc1e9eb43d745f70dfc55eaa240f41dab3c20db1
Christopher Leja <christopher.leja@gmail.com>
2020-01-28 19:37:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    adding_matz = programmer_hash[:yukihiro_matsumoto] = {
+    programmer_hash[:yukihiro_matsumoto] = {
       :known_for => "Ruby", :languages => ["LISP", "C"]}
-    adding_matz
     
 end
 
