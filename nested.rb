c6cfeaa4f3c7caf3faac0d307e4e80c50a55787e
Christopher Leja <christopher.leja@gmail.com>
2020-01-28 18:26:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,5 +1,6 @@
 
 def hopper
+  programmer_hash[:grace_hopper][:known_for]
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -21,7 +22,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:known_for]
+	programmer_hash[:alan_kay][:known_for]
 	
 	programmer_hash = 
  		{
@@ -59,6 +60,7 @@ def dennis_ritchies_language
 end
 
 def adding_matz
+  programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
